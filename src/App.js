38bd6392a4b23b6bf6ba5702120514f8c84a08c5
vicdubbeld <vicdubbeld@gmail.com>
2021-11-05 21:48:32 -0700

    created chat and admin user

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 
 //App component
 const App = () => {
-  return <ChatEngine height="100vh" projectID="" userName="" userSecret="" />;
+  return <ChatEngine height="100vh" projectID="1f74c2e-6647-4b6d-81ab-d4b9abeaa48e" userName="victor-dubbeld" userSecret="1234" />;
 };
 
 export default App;
