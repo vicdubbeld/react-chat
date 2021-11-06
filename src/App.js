7e56ce3a8e41d8adb7cd050cf0fbfa8fdfb2f8ec
vicdubbeld <vicdubbeld@gmail.com>
2021-11-05 21:43:21 -0700

    chat engine App component created.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
 import { ChatEngine } from "react-chat-engine";
 
 import "./App.css";
+
+//App component
+const App = () => {
+  return <ChatEngine height="100vh" projectID="" userName="" userSecret="" />;
+};
+
+export default App;
