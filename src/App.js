15777305cb8ae07171a5017fc68079010869c149
vicdubbeld <vicdubbeld@gmail.com>
2021-11-05 22:18:31 -0700

    chat room enabled

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import "./App.css";
 
 //App component
 const App = () => {
-  return <ChatEngine height="100vh" projectID="1f74c2e-6647-4b6d-81ab-d4b9abeaa48e" userName="victor-dubbeld" userSecret="1234" />;
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="11f74c2e-6647-4b6d-81ab-d4b9abeaa48e"
+      userName="victorD"
+      userSecret="12341234"
+    />
+  );
 };
 
 export default App;
