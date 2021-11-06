ffb85bae39a9001f121d197ac1c1b36aaa91ffde
vicdubbeld <vicdubbeld@gmail.com>
2021-11-05 22:47:34 -0700

    check if there are messages, find last message

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,5 +1,26 @@
+import MessageForm from "./ChatFeed";
+import MyMessage from "./MyMessage";
+import TheirMessage from "./TheirMessage";
+
+
+
 const ChatFeed = (props) => {
-    console.log(props)
+    const { chats, activeChat, userName, messages } = props
+
+    const chat = chats && chats[activeChat]
+
+    const renderMessages = () => {
+        const keys = Object.keys(messages);
+
+        return keys.map((key, index) => {
+            const message = messages[key];
+            //if there are messages, find last message
+            const lastMessageKey = index === 0 ? null : keys[index - 1]
+            const isMyMessage = userName === message.sender.username;
+        })
+    }
+
+    renderMessages()
 
     return (
         <div>
