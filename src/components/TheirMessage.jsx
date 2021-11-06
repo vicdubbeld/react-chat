a75870ae7d6c5b8584b27194bd1e86433b2080f8
vicdubbeld <vicdubbeld@gmail.com>
2021-11-05 23:16:58 -0700

    chatfeed logic styling added

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -15,10 +15,10 @@ const TheirMessage = ({ lastMessage, message }) => {
                                 src={message.attachments[0].file}
                                 alt='message-attachment'
                                 className="message-image"
-                                style={{float:'right'}}
+                                style={{marginLeft: isFirstMessageByUser ? '4px' : '48px'}}
                             />
                         ) : (
-                            <div className="message" style={{float:'right', marginRight: '18px', color:'white', backgroundColor: '#3B2A50' }}>
+                            <div className="message" style={{float:'left', backgroundColor: '#CABCDC', marginLeft: isFirstMessageByUser ? '4px' : '48px' }}>
                                 {message.text}
                             </div>
                         )
