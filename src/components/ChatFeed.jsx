1176ec3e284403fec97fcbd3d0195de0415dfe60
vicdubbeld <vicdubbeld@gmail.com>
2021-11-05 22:52:16 -0700

    read receipts added

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -17,6 +17,21 @@ const ChatFeed = (props) => {
             //if there are messages, find last message
             const lastMessageKey = index === 0 ? null : keys[index - 1]
             const isMyMessage = userName === message.sender.username;
+
+            return (
+                <div key={`msg_${index}`} style={{ width: '100%'}}>
+                    <div className="message-block">
+                        {
+                            isMyMessage
+                            ? <MyMessage />
+                            : <TheirMessage />
+                        }
+                    </div>
+                    <div className="read-receipts" style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px'}}>
+                    read-receipts
+                    </div>
+                </div>
+            )
         })
     }
 
