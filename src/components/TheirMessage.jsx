40ecab4811f08e250dd6bf8fb88237ae8d721523
vicdubbeld <vicdubbeld@gmail.com>
2021-11-05 23:13:34 -0700

    added turnary logic for rendering chat user info

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,9 +1,32 @@
-const TheirMessage = () => {
+const TheirMessage = ({ lastMessage, message }) => {
+    const isFirstMessageByUser = !lastMessage || lastMessage.sender.username != message.sender.username
+
     return(
-        <div>
-            TheirMessage
+        <div className="message-row">
+            {isFirstMessageByUser && (
+                <div 
+                    className='messageAvatar'
+                    style={{backgroundImage: `url(${message?.sender?.avatar})`}}
+                />
+            )}
+            {message?.attachments?.length > 0
+                ? (
+                            <img
+                                src={message.attachments[0].file}
+                                alt='message-attachment'
+                                className="message-image"
+                                style={{float:'right'}}
+                            />
+                        ) : (
+                            <div className="message" style={{float:'right', marginRight: '18px', color:'white', backgroundColor: '#3B2A50' }}>
+                                {message.text}
+                            </div>
+                        )
+            }
         </div>
     )
 }
+
+
 
 export default TheirMessage;
