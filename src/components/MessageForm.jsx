2d6eef36cbda12a56b2f2e9f8a163cc02ab651a3
vicdubbeld <vicdubbeld@gmail.com>
2021-11-09 08:33:04 -0800

    added ability to upload images to chat

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,14 +1,28 @@
 import { useState } from 'react'
-
-const MessageForm = () => {
+import { sendMessage, isTyping } from 'react-chat-engine'
+import { SendOutlined, PictureOutlined } from '@ant-design/icons'
+const MessageForm = (props) => {
     const [value, setValue] = useState('')
+    const { chatId, creds } = props
 
+    const handleSubmit = (event) => {
+        event.preventDefault()
 
-    const handleSubmit = () => {
+        const text = value.trim()
 
+        if(text.length > 0) {
+            sendMessage(creds, chatId, { text })
+        }
+        setValue('')
     }
-    const handleChange = () => {
+    const handleChange = (event) => {
+        setValue(event.target.value)
 
+        isTyping(props, chatId)
+    }
+
+    const handleUpload = (event) => {
+        sendMessage(creds, chatId, { files: event.target.files, text: '' })
     }
     return(
         <form className="message-form" onSubmit={handleSubmit}>
@@ -19,6 +33,21 @@ const MessageForm = () => {
                 onChange={handleChange}
                 onSubmit={handleSubmit}
              />
+            <label htmlFor="upload-button">
+                <span className="image-button">
+                    <PictureOutlined className="picture-icon"/>
+                </span>
+            </label>
+            <input 
+                type="file"
+                multiple={false}
+                id="upload-button"
+                style={{ display: 'none' }}
+                onChange={handleUpload.bind(this)}
+            />
+            <button type="submit" className="send-button">
+                <SendOutlined className="send-icon" />
+            </button>
         </form>
     )
 }
